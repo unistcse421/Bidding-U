34d82f99ed22098c3dd123b6d9ae067d031dc24d
JINwonLEE <jwl1993@unist.ac.kr>
2016-05-17 02:32:38 +0900

    bidding_list,add

--- mysite/bidding/urls.py
+++ mysite/bidding/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	#url(r"^$", views.index, name='bidding'),
+	url(r"^bidding_list/", views.bidding_list, name='bidding_list'),
+	url(r"^add_bidding/", views.add_bidding, name='add_bidding'),
+	
 ]

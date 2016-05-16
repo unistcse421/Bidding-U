456f6266ea8dce75b4bd0b6e9b81e1580b8004b4
juyoung0 <ojmme625@gmail.com>
2016-05-17 01:16:24 +0900

    add button

--- mysite/auction/views.py
+++ mysite/auction/views.py
@@ -3,12 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 
 def auction(request):
-	return render(request, 'auction/main.html', {})
+	if(request.GET.get('itemadd')):
+	        return render(request, 'auction/additem.html', {})
+	else:
+		return render(request, 'auction/main.html', {})
 
 def add_item(request):
-	return render(request, 'auction/additem.html', {})
+        if(request.GET.get('add')):
+                return render(request, 'auction/main.html', {})
+        else:
+		return render(request, 'auction/additem.html', {})
 
 def add_auction(request):
-	return render(request, 'auction/addauction.html', {})
+        if(request.GET.get('add')):
+                return render(request, 'auction/main.html', {})
+        else:
+		return render(request, 'auction/addauction.html', {})
 
 

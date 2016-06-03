b4128f5cbdabbbc17e3ab90a43a4397fc2530278
juyoung0 <ojmme625@gmail.com>
2016-06-03 15:26:57 +0900

    add ongoing and soldout button

--- mysite/bidding/urls.py
+++ mysite/bidding/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
 	url(r"^(?P<auction_id>[0-9]+)/$", views.bidding_list, name='biddinglist'),
 	url(r"^(?P<auction_id>[0-9]+)/addbidding/$", views.add_bidding, name='addbidding'),
-
+	url(r"^(?P<item_id>[0-9]+)/$", views.bidding_list, name='biddinglist'),
 ]
